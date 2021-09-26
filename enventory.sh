2c1fe3a4eec70843b124f9ffbafc4d50f2d97874
xalvaine <xalvaine@gmail.com>
2021-09-26 13:52:43 +0300

    feat: improve docker compose config

--- enventory.sh
+++ enventory.sh
@@ -8,8 +8,8 @@ echo "Версия операционной системы: $(source /etc/os-rel
 echo "Модель процессора:$(lscpu | grep "Model name" | tr -s ' ' | cut -d":" -f 2)"
 echo "Частота процессора:$(lscpu | grep "CPU MHz" | tr -s ' ' | cut -d":" -f 2)"
 echo "Объем физической памяти:$(lshw -c memory | grep size | cut -d":" -f 2)"
-echo "Название модели материнской платы:"
-echo "Версию и дату выпуска BIOS:"
+echo "Название модели материнской платы: недоступен в докере"
+echo -e "Версию и дату выпуска BIOS:\n$(dmidecode --type bios | grep 'Version\|Release Date')"
 echo "Перечень всех устройств, подключённых к шине PCI:
 $(lspci)"
 echo
