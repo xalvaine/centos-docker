55622e1c8891ea7bdf540fc867a448f73a8336c0
straivi <bme15122000@gmail.com>
2021-11-21 21:24:09 +0300

    osi 3 lab fix

--- enventory.sh
+++ enventory.sh
@@ -6,24 +6,25 @@ if ! [[ ( $1 =~ $re ) || ( $2 =~ $re ) ]] ; then
 fi
 mkdir -p /home/web_project/dev_staff
 mkdir -p /home/web_project/des_staff
-getent group staff || groupadd staff
-getent group developers || groupadd developers
-getent group designers || groupadd designers
+groupadd staff
+groupadd developers
+groupadd designers
 n=$(($1))
 m=$(($2))
 useradd -d /home/web_project project_manager
-project_manager:pm12345 | chpasswd
-for i in {1..n}
+echo "pm12345" | passwd project_manager --stdin
+usermod -a -G staff project_manager
+for (( i=1; i<=n; i++ ))
 do
-  useradd -d /home/web_project dev_user$(i)
-  dev$(i)12345 | passwd dev_user$(i) --stdin
-  usermod -a -G staff dev_user$(i)
-  usermod -a -G developers dev_user$(i)
+  useradd -d /home/web_project "dev_user${i}"
+  echo "dev${i}12345" | passwd "dev_user${i}" --stdin
+  usermod -a -G staff "dev_user${i}"
+  usermod -a -G developers "dev_user${i}"
 done
-for i in {1..m}
+for (( i=1; i<=m; i++ ))
 do
-  useradd -d /home/web_project des_user$(i)
-  des$(i)12345 | passwd des_user$(i) --stdin
-  usermod -a -G staff des_user$(i)
-  usermod -a -G developers des_user$(i)
+  useradd -d /home/web_project "des_user${i}"
+  echo "des${i}12345" | passwd "des_user${i}" --stdin
+  usermod -a -G staff "des_user${i}"
+  usermod -a -G designers "des_user${i}"
 done
