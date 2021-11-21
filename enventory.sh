3b01795891d5d504111a60c66975f104523f0943
straivi <bme15122000@gmail.com>
2021-11-21 20:47:29 +0300

    osi 3 lab

--- enventory.sh
+++ enventory.sh
@@ -1,15 +1,29 @@
-echo
-echo "Дата и время составления отчета: $(date)"
-echo "Пользователь, сгенерировавший отчет: $(whoami)"
-echo "Имя хоста: $(uname -n)"
-echo "Название ядра: $(uname -s)"
-echo "Версия ядра: $(uname -r)"
-echo "Версия операционной системы: $(source /etc/os-release; echo $NAME $VERSION)"
-echo "Модель процессора:$(lscpu | grep "Model name" | tr -s ' ' | cut -d":" -f 2)"
-echo "Частота процессора:$(lscpu | grep "CPU MHz" | tr -s ' ' | cut -d":" -f 2)"
-echo "Объем физической памяти:$(lshw -c memory | grep size | cut -d":" -f 2)"
-echo "Название модели материнской платы: недоступен в докере"
-echo -e "Версию и дату выпуска BIOS:\n$(dmidecode --type bios | grep 'Version\|Release Date')"
-echo "Перечень всех устройств, подключённых к шине PCI:
-$(lspci)"
-echo
+
+re='^[0-9]+$'
+if ! [[ ( $1 =~ $re ) || ( $2 =~ $re ) ]] ; then
+   echo "error: Not a number" >&2
+   exit 1
+fi
+mkdir -p /home/web_project/dev_staff
+mkdir -p /home/web_project/des_staff
+getent group staff || groupadd staff
+getent group developers || groupadd developers
+getent group designers || groupadd designers
+n=$(($1))
+m=$(($2))
+useradd -d /home/web_project project_manager
+project_manager:pm12345 | chpasswd
+for i in {1..n}
+do
+  useradd -d /home/web_project dev_user$(i)
+  dev$(i)12345 | passwd dev_user$(i) --stdin
+  usermod -a -G staff dev_user$(i)
+  usermod -a -G developers dev_user$(i)
+done
+for i in {1..m}
+do
+  useradd -d /home/web_project des_user$(i)
+  des$(i)12345 | passwd des_user$(i) --stdin
+  usermod -a -G staff des_user$(i)
+  usermod -a -G developers des_user$(i)
+done
