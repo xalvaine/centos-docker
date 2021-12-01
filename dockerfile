df2c9164025e7d566284b45c3bbe1893ce768b6b
straivi <bme15122000@gmail.com>
2021-12-01 16:36:26 +0300

    add requirement from task

--- dockerfile
+++ dockerfile
@@ -2,3 +2,5 @@ FROM centos
 
 RUN yum install sudo -y
 RUN yum install net-tools -y
+RUN yum install NetworkManager -y
+RUN yum install util-linux -y
--- enventory.sh
+++ enventory.sh
@@ -16,15 +16,17 @@ echo "pm12345" | passwd project_manager --stdin
 usermod -a -G staff project_manager
 for (( i=1; i<=n; i++ ))
 do
-  useradd -d /home/web_project "dev_user${i}"
+  useradd -d "/home/web_project/dev_user${i}" -e $(date -d '+30 days' '+%Y-%m-%d') "dev_user${i}"
   echo "dev${i}12345" | passwd "dev_user${i}" --stdin
   usermod -a -G staff "dev_user${i}"
   usermod -a -G developers "dev_user${i}"
+  chage -W 3 -M -7 "dev_user${i}"
 done
 for (( i=1; i<=m; i++ ))
 do
-  useradd -d /home/web_project "des_user${i}"
+  useradd -d "/home/web_project/des_user${i}" -e $(date -d '+30 days' '+%Y-%m-%d') "des_user${i}"
   echo "des${i}12345" | passwd "des_user${i}" --stdin
   usermod -a -G staff "des_user${i}"
   usermod -a -G designers "des_user${i}"
+  chage -W 3 -M -7 "des_user${i}"
 done
